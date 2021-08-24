b9c4ccda6a7db4a15868bd7d3535f934b398e948
Norman Munge <munge@sisitech.com>
2021-08-24 09:42:41 +0300

    Edits banner-slide component

--- src/app/banner-slide/banner-slide.component.ts
+++ src/app/banner-slide/banner-slide.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banner-slide',
   templateUrl: './banner-slide.component.html',
-  styleUrls: ['./banner-slide.component.scss']
+  styleUrls: ['./banner-slide.component.scss'],
 })
 export class BannerSlideComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  public frontEndSkills = [
+    'Html',
+    '(S)Css',
+    'TailwindCss',
+    'Javascript',
+    // 'Typescript',
+    'Angular',
+    'EmberJs',
+    'Api Integration',
+  ];
 
+  public backEndSkills = [
+    'Python',
+    'Django',
+    'Php',
+    'Laravel',
+    'NodeJs',
+    'PostgresSql',
+    'MySql',
+    // 'Linux'
+  ];
 }
